011e706e0f414c92bd3820cf18b4e0fdae4c3478
Maya Saietz <ms@tec.dk>
2023-04-15 10:27:21 +0200

    Player script part 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+
+    private PlayerInput input;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        input = new PlayerInput();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
