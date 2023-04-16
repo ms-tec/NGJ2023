feb123aca11dacd075048bf40f4bfb3bea4d3d8a
Jakob Scheunemann Hastrup <jsha@tec.dk>
2023-04-16 09:58:27 +0200

    Swear moves against players position at spawn

--- Assets/Scripts/SwearWords/SwearController.cs
+++ Assets/Scripts/SwearWords/SwearController.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class SwearController : MonoBehaviour
 {
-    
+    public GameObject player;
+    private Vector3 playerPos;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        playerPos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, playerPos, step);
     }
 }
